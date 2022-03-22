94c060f5c1cb6a998ee32488c74b40caa59ab476
Henrique Paulino Mendes Lima <henriquesax@gmail.com>
2022-03-22 17:54:21 -0300

    Banco de dados H2 e criação automática da base de dados

---
+++ src/main/java/com/example/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,9 @@
+package com.example.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+}

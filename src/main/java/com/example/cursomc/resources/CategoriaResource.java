1e21b8597787891c98a5a163c07407e7ef8cafd9
Henrique Paulino Mendes Lima <henriquesax@gmail.com>
2022-03-22 18:26:29 -0300

    Repository e Service de CAtegoria

--- src/main/java/com/example/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/example/cursomc/resources/CategoriaResource.java
@@ -1,9 +1,23 @@
 package com.example.cursomc.resources;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
+
+    @Autowired
+    private CategoriaService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id){
+        Categoria obj = service.buscar(id);
+        return ResponseEntity.ok().body(obj);
+    }
 }
